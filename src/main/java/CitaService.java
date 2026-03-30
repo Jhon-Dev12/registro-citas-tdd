64c3ea46d9d1797b3508632accbeaf100bcc95c1
Jhon <jhonramos3456@gmail.com>
2026-03-30 09:13:18 -0500

    Implementacion minima para el test 1

--- src/main/java/CitaService.java
+++ src/main/java/CitaService.java
@@ -3,6 +3,6 @@ import java.time.LocalDate;
 public class CitaService {
 
     public static String validar(String c123, String carlos, String number, LocalDate localDate) {
-        return "";
+        return "La cita ha sido registrada correctamente";
     }
 }
